ef8a92eef8ade8f056ab3bf7b82ab8c1fce2c217
Filip Wall <filip.wall@lund.ntig.se>
2017-11-14 14:56:22 +0100

    Deleted everything that did not belong to ASP.net CORE v.2

--- bitsoccer/BitSoccerEngine/Common/Internal/CodeException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class CodeException : SystemException
-{
-    public CodeException(string assembly, string type)
-        : base("Invalid code in " + assembly + " in type " + type)
-    {
-    }
-}
--- bitsoccerweb/bitsoccer.net/bitsoccer.net/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bitsoccer.net.MvcApplication" Language="C#" %>
