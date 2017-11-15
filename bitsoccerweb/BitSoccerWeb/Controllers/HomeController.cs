7129743b435576b1a1c60f0e2ac095240313294d
OskarMolander <oskar.molander@lund.ntig.se>
2017-11-15 11:13:39 +0100

    Contact page removed

--- bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
+++ bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
@@ -21,14 +21,6 @@ namespace BitSoccerWeb.Controllers
 
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
