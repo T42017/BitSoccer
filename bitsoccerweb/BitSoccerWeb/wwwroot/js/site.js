31de6bddbbff7eab4b765d6a52787ee731a99dc3
pontuslaos <pontus.laos@lund.ntig.se>
2017-11-20 14:02:42 +0100

    Uploaded some pre-made Teams, added p5.js library.

--- bitsoccerweb/BitSoccerWeb/wwwroot/js/site.js
+++ bitsoccerweb/BitSoccerWeb/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your JavaScript code.
+
+var canvas;
+
+function setup() {
+    canvas = createCanvas();
+    canvas.class("col-md-8 col-md-offset-2");
+    canvas.style("background-color", "black");
+    canvas.parent("game-div");
+}
+
+function draw() {
+    fill(255);
+    rect(0, 0, 50, 50);
+}
