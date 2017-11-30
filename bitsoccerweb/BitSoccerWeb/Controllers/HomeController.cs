b86535fea352076cd77bb78ee94e9c05f778764b
OskarMolander <oskar.molander@lund.ntig.se>
2017-11-30 15:28:59 +0100

    added method post to submit button on simulate page

--- bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
+++ bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 
 namespace BitSoccerWeb.Controllers
@@ -59,6 +61,41 @@ namespace BitSoccerWeb.Controllers
 
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult TwoTeamSim()
+        {
+            
+            var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            var teamOnePath = @"\bitsoccerweb\BitSoccerWeb\Teams\TeamOne.dll";
+            var teamOne = Path.Combine(projectFolder + teamOnePath);
+
+            var teamTwoPath = @"\bitsoccerweb\BitSoccerWeb\Teams\TeamLomma.dll";
+            var teamTwo = Path.Combine(projectFolder + teamTwoPath);
+
+            var matchPath = $@"\bitsoccerweb\BitSoccerWeb\Matches\{Guid.NewGuid()}.xml";
+            var matches = Path.Combine(projectFolder + matchPath);
+            var mPath = @"\bitsoccerweb\\BitSoccerWeb\Matches\f690a309-7bd6-4958-bb7f-47b943e8a099.xml";
+            var match2 = Path.Combine(projectFolder + mPath);
+           
+
+            MatchManager.PlayMatch(teamOne,teamTwo,matches);
+
+
+
+            //var document = XDocument.Load(match2);
+            //var root = document.Root;
+            //var lastElement = (XElement) root.;
+
+            //var document = XDocument.Load(match2);
+            //var text = document.XPathSelectElements("//@TeamScores");
+
+            //Debug.WriteLine(text.ToString());
+
+
+            return View("../Home/Simulate");
+
         }
 
         public IActionResult Error()
