8fddea2d9025bbe0c1e699ca0870439ad54053e9
OskarMolander <oskar.molander@lund.ntig.se>
2017-12-18 11:15:28 +0100

    Fixed dropdown on index page. The stuff done in HomeController does not wok.

--- bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
+++ bitsoccerweb/BitSoccerWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -16,6 +17,8 @@ using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
+using BitSoccerWeb.Data;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace BitSoccerWeb.Controllers
@@ -60,11 +63,22 @@ namespace BitSoccerWeb.Controllers
 
             for (int i = 1; i < nrOfMatches+1; i++)
             {
-                var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-                var teamOnePath = @"\bitsoccerweb\BitSoccerWeb\Teams\TeamScania.dll";
-                var teamOne = Path.Combine(projectFolder + teamOnePath);
+                string t1;
+                string t2;
+                using (var context = new ApplicationDbContext(null))
+                {
+                    var team1NameFromDb = context.Teams.First(t => t.Id == TeamOne);
+                    var team2NameFromDb = context.Teams.First(t => t.Id == TeamTwo);
+                    t1 = team1NameFromDb.FilePath;
+                    t2 = team2NameFromDb.FilePath;
+                }
 
-                var teamTwoPath = @"\bitsoccerweb\BitSoccerWeb\Teams\TeamOskar.dll";
+
+                var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                var teamOnePath = $@"\bitsoccerweb\BitSoccerWeb\Teams\{t1}";
+                var teamOneFullPath = Path.Combine(projectFolder + teamOnePath);
+
+                var teamTwoPath = $@"\bitsoccerweb\BitSoccerWeb\Teams\{t2}";
                 var teamTwo = Path.Combine(projectFolder + teamTwoPath);
 
                 var firstSplit = teamOnePath.Split(@"Teams\");
@@ -78,7 +92,7 @@ namespace BitSoccerWeb.Controllers
                 var matches = Path.Combine(projectFolder + matchPath);
                 
 
-                MatchManager.PlayMatch(teamOne, teamTwo, matches);
+                MatchManager.PlayMatch(teamOneFullPath, teamTwo, matches);
                 var document = XDocument.Load(matches);
                 var teamOneScore = document.XPathSelectElements("//SerializableGameState").Last().FirstAttribute.Value;
                 var teamTwoScore = document.XPathSelectElements("//SerializableGameState").Last().LastAttribute.Value;
